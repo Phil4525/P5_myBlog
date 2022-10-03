8a7b461f9970663146825413578c6daeed372fb8
Phil4525 <philippe.mariou70@gmail.com>
2022-10-03 10:49:44 +0200

    test $var

--- templates/blog.php
+++ templates/blog.php
@@ -5,7 +5,7 @@ use App\Lib\Helpers;
 $title = "myBlog";
 ob_start();
 require 'headers/blog.php';
-
+$frenchCreationDate = Helpers::esc_html($featuredPost->frenchCreationDate);
 // use App\Lib\Helpers;
 ?>
 <!-- Page content-->
@@ -17,7 +17,7 @@ require 'headers/blog.php';
             <div class="card mb-4">
                 <a href="index.php?action=post&id=<?= urlencode($featuredPost->id) ?>"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..." /></a>
                 <div class="card-body">
-                    <div class="small text-muted"><?= Helpers::esc_html($featuredPost->frenchCreationDate) ?></div>
+                    <div class="small text-muted"><?= $frenchCreationDate ?></div>
                     <h2 class="card-title"><?= htmlentities($featuredPost->title) ?></h2>
                     <p class="card-text"><?= $featuredPost->chapo ?></p>
                     <div class="d-flex justify-content-end">
